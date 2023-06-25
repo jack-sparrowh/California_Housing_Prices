b212d76c56d4f2a144a71ad9139ab1cbf66aabdf
jack-sparrowh <107581485+jack-sparrowh@users.noreply.github.com>
2023-06-25 21:23:14 +0200

    minor changes, model testing
    i have finally started testing models

--- classes_for_pipeline_py.py
+++ classes_for_pipeline_py.py
@@ -1,6 +1,8 @@
 import numpy as np
 import pandas as pd
 from sklearn.base import BaseEstimator, TransformerMixin
+from sklearn.impute import SimpleImputer
+from sklearn.preprocessing import PowerTransformer, StandardScaler
 from scipy.stats import iqr, skew
 
 def fast_pipeline(X):
@@ -830,7 +832,7 @@ class PreprocessPipeline(BaseEstimator, TransformerMixin):
         
         X = self.median_imputer.transform(X)
         X = self.feature_adder.fit_transform(X)
-        X = self.outliers_dropper.transform(X)
+        X = self.outliers_dropper.fit_transform(X)
         X = X.dropna()
         X = self.standardizer.transform(X)
         X = self.power_transformer.transform(X)
